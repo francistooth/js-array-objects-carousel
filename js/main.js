0e3e0e24e1d5689ca36a3c65116da4601da5e9fd
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-19 15:23:54 +0200

    1.0.2: fix: thumbnail fix, text position fix, ex complete

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ const preview = document.getElementById('preview');
 const thumbs = document.getElementById('thumbs');
 
 images.forEach((element) => {
+    // element creation
+
     const preDiv = document.createElement('div');
 
     if (element === 'img/01.webp') 
@@ -17,13 +19,47 @@ images.forEach((element) => {
 
     preDiv.classList.add('item', 'position-relative');
 
+    // img
+
     const img = document.createElement('img');
 
     img.src = element.image
 
     preDiv.appendChild(img);
 
+    // text
+
+    const textDiv = document.createElement('div');
+    textDiv.classList.add('item-text');
+
+    const title = document.createElement('h3');
+    title.innerHTML = element.title;
+
+    const para = document.createElement('p');
+    para.innerHTML = element.text;
+
+    textDiv.appendChild(title);
+    textDiv.appendChild(para);
+
+    preDiv.appendChild(textDiv);
+
     preview.appendChild(preDiv);
+
+    // thumbs 
+
+    const thumbDiv = document.createElement('div');
+
+    thumbDiv.classList.add('th-item');
+
+    // img
+
+    const thumbImg = document.createElement('img');
+
+    thumbImg.src = element.image;
+
+    thumbDiv.appendChild(thumbImg);
+
+    thumbs.appendChild(thumbDiv);
 });
 
 let slideIndex = 1;
